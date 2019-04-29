3ccd5b153e17710a5cac7f6abd3b51388aebff03
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-04-29 16:26:55 +0300

    added identifier template (incorrect state)

---
+++ GraphConfig/Config/IdentifierTemplate.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace GraphConfig.Config
+{
+    public class IdentifierPartTemplate
+    {
+        public string Name { get; }
+        public string BeginTemplate { get; }
+        public string EndTemplate { get; }
+
+        [JsonConstructor]
+        public IdentifierPartTemplate(string name, string beginTemplate, string endTemplate)
+        {
+            Name = name;
+            BeginTemplate = beginTemplate;
+            EndTemplate = endTemplate;
+        }
+    }
+}
