3eb24497f74b3975e2ae39c59471641c192c4492
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-04-29 16:07:41 +0300

    added tool window (no code written)

---
+++ GraphAlgorithmRenderer/SettingsWindow.cs
@@ -0,0 +1,34 @@
+﻿namespace GraphAlgorithmRenderer
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("52a96019-a6a1-4390-a42a-d6f3b1e160cc")]
+    public class SettingsWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SettingsWindow"/> class.
+        /// </summary>
+        public SettingsWindow() : base(null)
+        {
+            this.Caption = "SettingsWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new SettingsWindowControl();
+        }
+    }
+}
