33ec4e52da5156f9f905a968f7f2a64de43ea2c9
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-04-29 17:19:18 +0300

    added renderer to plugin, doesn't work

--- GraphAlgorithmRenderer/SettingsWindow.cs
+++ GraphAlgorithmRenderer/SettingsWindow.cs
@@ -1,6 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using EnvDTE;
 using GraphConfig.Config;
 using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.GraphViewerGdi;
+using Microsoft.VisualStudio.Shell;
+using Condition = GraphConfig.Config.Condition;
+using GraphRenderer = GraphConfig.GraphRenderer.GraphRenderer;
+using Process = EnvDTE.Process;
+using Size = System.Drawing.Size;
+using StackFrame = EnvDTE.StackFrame;
 
 namespace GraphAlgorithmRenderer
 {
@@ -34,10 +46,72 @@ namespace GraphAlgorithmRenderer
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
             // the object returned by the Content property.
             this.Content = new SettingsWindowControl();
+        }
+
+        protected override void Initialize()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var applicationObject = (DTE)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(DTE));
             _config = CreateGraphConfig();
+            _debugEvents = applicationObject.Events.DebuggerEvents;
+            _debugEvents.OnContextChanged +=
+                Update;
+            var debugger = applicationObject.Debugger;
+
+            _renderer = new GraphRenderer(_config,
+                debugger);
+
+            _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += dispatcherTimer_Tick;
+            _dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 50);
+            _dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (!_shouldBeRedrawn)
+            {
+                return;
+            }
+
+            DrawGraph();
+            _shouldBeRedrawn = false;
+        }
+
+        private void Update(Process newprocess, Program newprogram, Thread newthread, StackFrame newstackframe)
+        {
+            _shouldBeRedrawn = true;
         }
 
         private GraphConfig _config;
+        private Form _form;
+        private GraphRenderer _renderer;
+        private DebuggerEvents _debugEvents;
+        private bool _shouldBeRedrawn = true;
+        private DispatcherTimer _dispatcherTimer;
+
+
+        private void DrawGraph()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Graph graph = _renderer.RenderGraph();
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds / 10:00}";
+            Debug.WriteLine(elapsedTime);
+            GViewer viewer = new GViewer { Graph = graph, Dock = DockStyle.Fill };
+            if (_form == null)
+            {
+                _form = new Form {Size = new Size(800, 800)};
+            }
+
+            _form.SuspendLayout();
+            _form.Controls.Clear();
+            _form.Controls.Add(viewer);
+            _form.ResumeLayout();
+            _form.Show();
+        }
 
         public GraphConfig CreateGraphConfig()
         {
