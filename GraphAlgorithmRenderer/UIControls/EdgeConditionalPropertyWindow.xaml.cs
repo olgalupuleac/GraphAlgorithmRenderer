5f84f3fe48719b89afdd3a9f6496135d21c04a7a
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-05-06 14:45:47 +0300

    added ConditionalProperty generator to EdgeConditionalPropertyWindow

--- GraphAlgorithmRenderer/UIControls/EdgeConditionalPropertyWindow.xaml.cs
+++ GraphAlgorithmRenderer/UIControls/EdgeConditionalPropertyWindow.xaml.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using GraphAlgorithmRenderer.Config;
+using static System.Single;
+using Condition = GraphAlgorithmRenderer.Config.Condition;
+using Style = Microsoft.Msagl.Drawing.Style;
 
 namespace GraphAlgorithmRenderer.UIControls
 {
@@ -21,6 +16,18 @@ namespace GraphAlgorithmRenderer.UIControls
     /// </summary>
     public partial class EdgeConditionalPropertyWindow : Window
     {
+        private readonly IReadOnlyDictionary<string, Microsoft.Msagl.Drawing.Style> _styles = new Dictionary<string, Style>
+        {
+            {"Bold", Microsoft.Msagl.Drawing.Style.Bold},
+            {"Dashed", Microsoft.Msagl.Drawing.Style.Dashed},
+            {"Diagonals", Microsoft.Msagl.Drawing.Style.Diagonals},
+            {"Dotted", Microsoft.Msagl.Drawing.Style.Dotted},
+            {"Filled", Microsoft.Msagl.Drawing.Style.Filled},
+            {"Invis", Microsoft.Msagl.Drawing.Style.Invis},
+            {"Rounded", Microsoft.Msagl.Drawing.Style.Rounded},
+            {"Solid", Microsoft.Msagl.Drawing.Style.Solid}
+        };
+
         public EdgeConditionalPropertyWindow()
         {
             InitializeComponent();
@@ -41,6 +48,7 @@ namespace GraphAlgorithmRenderer.UIControls
                     box.IsEnabled = false;
                 }
             }
+
             styleCheckBox.Checked += StyleCheckBoxOnChecked;
             styleCheckBox.Unchecked += StyleCheckBoxOnUnchecked;
             lineWidthCheckBox.Checked += (sender, args) => lineWidthBox.IsEnabled = true;
@@ -76,8 +84,8 @@ namespace GraphAlgorithmRenderer.UIControls
 
         protected override void OnClosing(CancelEventArgs e)
         {
-            e.Cancel = true;  // cancels the window close    
-            Hide();      // Programmatically hides the window
+            e.Cancel = true; // cancels the window close    
+            Hide(); // Programmatically hides the window
         }
 
         private void LabelCheckBoxOnUnchecked(object sender, RoutedEventArgs e)
@@ -97,6 +105,56 @@ namespace GraphAlgorithmRenderer.UIControls
         private void Ok_Click(object sender, RoutedEventArgs e)
         {
             Hide();
+        }
+
+        public ConditionalProperty<IEdgeProperty> ConditionalProperty
+        {
+            get
+            {
+                var checkedRadioButton = ModePanel.Children.OfType<RadioButton>()
+                    .FirstOrDefault(r => r.IsChecked != null && (bool) r.IsChecked);
+                var mode = ConditionMode.CurrentStackFrame;
+                if (checkedRadioButton == AllSf)
+                {
+                    mode = ConditionMode.AllStackFrames;
+                }
+
+                var condition = new Condition(conditionBox.Text,
+                    regexBox.Text, mode);
+                var properties = new List<IEdgeProperty>();
+                if (labelCheckBox.IsChecked != null && (bool) labelCheckBox.IsChecked)
+                {
+                    var labelProperty = new LabelEdgeProperty(labelTextBox.Text);
+                    if (TryParse(labelFontSizeBox.Text, out var f))
+                    {
+                        labelProperty.FontSize = f;
+                    }
+
+                    properties.Add(labelProperty);
+                }
+
+                if (colorCheckBox.IsChecked != null && (bool) colorCheckBox.IsChecked)
+                {
+                    var color = colorPicker.SelectedColor;
+                    properties.Add(new LineColorEdgeProperty(
+                        new Microsoft.Msagl.Drawing.Color(a: color.Value.A,
+                            r: color.Value.R, g: color.Value.G, b: color.Value.B)));
+                }
+
+                if (lineWidthCheckBox.IsChecked != null && (bool) lineWidthCheckBox.IsChecked)
+                {
+                    properties.Add(new LineWidthEdgeProperty(Parse(labelFontSizeBox.Text)));
+                }
+
+                if (styleCheckBox.IsChecked != null && (bool) styleCheckBox.IsChecked)
+                {
+                    ModePanel.Children.OfType<CheckBox>()
+                        .Where(cb => cb.IsChecked != null && (bool) cb.IsChecked)
+                        .ToList().ForEach(cb => properties.Add(new StyleEdgeProperty(_styles[cb.ContentStringFormat])) );
+                }
+
+                return new ConditionalProperty<IEdgeProperty>(condition, properties);
+            }
         }
     }
 }
