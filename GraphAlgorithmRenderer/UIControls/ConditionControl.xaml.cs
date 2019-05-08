ea405cbdb90fec046505cd697158b86982347447
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-05-08 15:25:42 +0300

    added Description to ConditionalControl

--- GraphAlgorithmRenderer/UIControls/ConditionControl.xaml.cs
+++ GraphAlgorithmRenderer/UIControls/ConditionControl.xaml.cs
@@ -1,20 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using GraphAlgorithmRenderer.Config;
-using Condition = System.Windows.Condition;
 
 namespace GraphAlgorithmRenderer.UIControls
 {
@@ -35,12 +21,22 @@ namespace GraphAlgorithmRenderer.UIControls
         {
             get
             {
-                ConditionMode mode = AllSf.IsChecked == true
-                    ? ConditionMode.AllStackFrames
-                    : ConditionMode.CurrentStackFrame;
+                var mode = GetMode();
                 return new GraphAlgorithmRenderer.Config.Condition(ConditionBox.Text, RegexBox.Text, mode);
             }
         }
+
+        public ConditionMode GetMode()
+        {
+            return  AllSf.IsChecked == true
+                ? ConditionMode.AllStackFrames
+                : ConditionMode.CurrentStackFrame;
+        }
+
+
+        public string Description =>
+            string.Join(" ", ConditionBox.Text, RegexBox.Text,
+                Enum.GetName(typeof(ConditionMode), GetMode()));
 
         public void FromCondition(GraphAlgorithmRenderer.Config.Condition condition)
         {
